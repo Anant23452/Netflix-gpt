db350727084cc7cf467f9bb805ef01e084bfdf79
anantkpatel <imagine231216@gmail.com>
2025-10-08 18:43:37 +0530

    nlk

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,6 +12,11 @@ function Login() {
             <img className='' src="https://www.techhive.com/wp-content/uploads/2023/10/Netflix-Hintergrund.jpg?quality=50&strip=all " alt="" />
             <form action="" className='bg-black-500 p-12  absolute top-1/4 left-1/3 flex flex-col gap-4 z-99 w-1/3 h-[70vh] bg-gray-800 bg-opacity-80 items-center '>
                 <h2 className='text-2xl font-bold text-white  '>{signup ? "Sign up" : "Sign in"} </h2>
+                {
+                    signup && (
+                        <input type="text" placeholder='Name' className='p-3 rounded-md px-10  bg-transparent border-2 placeholder:text-xl' />
+                    )
+                }
                 <input type="text" placeholder='Email' className='p-3 rounded-md px-10  bg-transparent border-2 placeholder:text-xl' />
                 <input type="text" placeholder='Password' className='p-3 rounded-md px-10 bg-transparent border-2 placeholder:text-xl  ' />
                 < button className='bg-red-600 text-white px-28 py-3 rounded-md text-xl font-bold  ' type='button' >Sign In</button>
