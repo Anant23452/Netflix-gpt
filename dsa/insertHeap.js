9800b2857f964b3ab8f868d5e81982a25a5ba334
anantkpatel <imagine231216@gmail.com>
2026-01-17 00:16:47 +0530

    k'p;

--- dsa/insertHeap.js
+++ dsa/insertHeap.js
@@ -31,9 +31,47 @@ class MinHeap{
             }
         }
     }
+
+    extractMin(){
+        if(this.heap.length<1)return null;
+        let min = this.heap[0];
+        let lastIndex = this.heap.length-1;
+        //swap
+        [this.heap[0],this.heap[lastIndex]]=[this.heap[lastIndex],this.heap[0]];
+        this.heap.pop();
+        this.heapifyDown(0);
+        return min;
+    }
+
+   heapifyDown(i){
+    let left = this.getLeftChildIndex(i);
+    let right = this.getRightChildIndex(i);
+    let smallest = i;
+    let n = this.heap.length;
+    if(left<n && left<this.heap[smallest]){
+        smallest = left;
+    }
+    if(right<n && right<this.heap[smallest]){
+        smallest = right;
+    }
+    if(smallest != i){
+        //swap
+        [this.heap[smallest],this.heap[i]]=[this.heap[i],this.heap[smallest]];
+        this.heapifyDown(smallest);
+    }
+   } 
+
+   peek(){
+    if(!this.heap.length)return null;
+    return this.heap[0]
+   }
 }
 
 let Heap = new MinHeap();
 console.log(Heap);
 Heap.intsert(3);
 console.log(Heap);
+Heap.extractMin();
+console.log(Heap);
+Heap.peek()
+console.log(peek);
